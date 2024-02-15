73e64c7d6a2a541cf512d9997cbebb09099c6af9
NCorbett1 <160158953+NCorbett1@users.noreply.github.com>
2024-02-15 08:58:18 -0800

    Hello Upload!

---
+++ main.py
@@ -0,0 +1,86 @@
+# This file was created by: Nick Corbett
+
+#Imports pygame as pg and imports settings code
+import pygame as pg 
+from settings import *
+from sprites import *
+from random import randint
+import sys
+from os import path
+#creates a variable called game
+class Game:
+#code template for creating python
+    def __init__(self):
+        # initiate self
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TILE)
+        #set game clock
+        self.clock = pg.time.Clock()
+        self.load_data()
+    def load_data(self):
+        pass
+   #create way to run whole game    
+    def new(self):
+    # sets size of player
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        self.player1= Player(self, 1, 1)
+        for x in range(10, 20):
+            Wall(self, x, 5)
+            for row, tiles in enumerate(self.map_data):
+                print(row)
+            for col, tile in enumerate(tiles):
+                print(col)
+                if tile == '1':
+                    print("a wall at", row, col)
+                    Wall(self, col, row)
+#why does this go there
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+#imports clock and settings
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+    def quit(self):
+        pg.quit()   
+        sys.exit()
+#will update sprites and changes
+    def update(self):
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+#parameters for game
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+        # if event.type == pg.KEYDOWN:
+        #     if event.key == pg.K_LEFT:
+        #         self.player1.move(dx=-1)
+#Instantiate the game...
+g = Game()
+#use game method run to run
+#g.show_start_screen
+while True:
+    g.new()
+    g.run()
+    #g.show_go_screen()
+
+
+        
---
+++ sprites.py
@@ -0,0 +1,87 @@
+# This file was created by: Nick Corbett
+# This code was inspired by Zelda and informed by Chris Bradfield
+import pygame as pg 
+from settings import *
+#Import pygame and settings
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        self.groups = game.all_sprites
+        # init super class
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y 
+#Shows how to
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys = [pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = PLAYER SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = PLAYER_SPEED
+        if keys[pg/K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx !=0 and self.vy !=0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
+
+#This code imports the sizes and basics of the player
+    def move(self, dx=0, dy=0):
+        if not self.collide_with_walls(dx, dy):
+         self.x += dx
+         self.y += dy
+    
+    def collide_with_walls(self, dx=0, dy=0)
+        for wall in self.game.walls:
+            if wall.x == slef.x + dx and wall.y == self.y +dy: 
+                return True
+            return False
+        
+#What happens if game collides with walls
+def collide_with_walls(self, dir):
+    if dir == 'x':
+        hits = pg.sprite.spritecollide(self, self.game.walls, False)
+        if hits: 
+            if self.vx > 0: 
+                self.x = hits[0].rect.left - self.width
+            if self.vx < 0: 
+                self.x = hits[0].rect.right
+            self.vx = 0
+            self.rect.x = self.x
+    if dir == 'y':
+        hits = pg.sprite.spritecollide(self, self.game.walls, False)
+        if hits: 
+            if self.vy > 0: 
+                self.y = hits[0].rect.top - self.rect.height
+            if self.vy < 0: 
+                self.y = hits[0].rect.bottom
+            self.vy = 0
+            self.rect.y = self.y
+
+#updates system
+    def update(self):
+        self.get_keys()
+        self.x += self.vx * self.game.dt 
+        self.y += self.vy * self.game.dt 
+        self.rect.x = x * TILESIZE
+        self.rect.y = y * TILESIZE
+        
+#Line 21 and below initiate the wall with sprites
+class Wall(pg.sprite.Sprite): 
+        def __init__(self, x, y):
+            self.groups = game.all_sprites, game.walls
+            pg.sprite.Sprite.__init__(self, self.groups)
+            self.image = pg.Surface((TILESIZE, TILESIZE))
+            self.image.fill(BLUE)
+            self.rect = self.image.get_rect()
+            self.x = x
+            self.y = y
+            self.rect.x = x * TILESIZE
+            self.rect.y = y * TILESIZE
