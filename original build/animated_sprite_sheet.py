8e463370c5e418457e45555f7500d6350e6acb28
NCorbett1 <160158953+NCorbett1@users.noreply.github.com>
2024-03-27 09:33:36 -0700

    animate sprites and sprite sheet

---
+++ original build/animated_sprite_sheet.py
@@ -0,0 +1,24 @@
+import pygame as pg
+
+FPS = 30
+
+frames = ["Frame 1", "Frame 2", "Frame 3", "Frame 4"]
+
+clock = pg.time.Clock()
+
+last_update = 0
+
+def animate():
+    global last_update 
+    global current_frame
+    now = pg.time.get_ticks()
+    if now - last_update > 350:
+        current_frame = (current_frame +1) % len(frames)
+        print(now)
+        last_update = now
+    print(now)
+
+while True: 
+    clock.tick(FPS)
+    animate()
+    
---
+++ original build/animated_sprites.py
@@ -0,0 +1,23 @@
+import time
+
+# while true: 
+#     time.sleep(1)
+#     print("Frame" + str((z)))
+#     z += 1
+
+
+    # time.sleep(1)
+    # print("Frame 112")
+    # time.sleep(2)
+    # print("Frame 2")
+    # time.sleep(3)
+    # print("Frame 3")
+    # time.sleep(4)
+    # print("Frame 4")
+
+i = 1
+while i>0:
+    mylist = ["Frame 1", "Frame 2", "Frame 3", "Frame 4"]
+    for m in mylist: 
+        time.sleep(1)
+        print(m)
